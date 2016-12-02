b9a843b44fe68279668f1072397b9c6b486f76db
Marcus Aurén <auren@student.chalmers.se>
2016-12-02 11:33:22 +0100

    MineSweeper started

---
+++ MineSweeper/MineSweeper.hs
@@ -0,0 +1,57 @@
+module MineSweeper where
+
+import Test.QuickCheck hiding (shuffle)
+import Data.Char (ord, chr, isDigit, digitToInt, intToDigit)
+import Data.Ord
+import System.Random
+import Data.List
+import Data.Maybe
+import System.Random.Shuffle
+
+--REMOVE!!!!
+import Debug.Trace
+debug = flip trace
+  --REMOVE!!!!
+
+data Tile = Bomb | Numeric Integer
+              deriving (Show, Eq)
+
+data MineField = MineField {rows :: [[Tile]]}
+              deriving ( Show, Eq )
+
+type Pos = (Int,Int)
+{-
+testFunction x y b = do g <- newStdGen
+                 return makeBombField b (makeEmptyField x y) g
+-}
+makeEmptyField :: Int -> Int -> MineField
+makeEmptyField x y = MineField {rows = replicate y (replicate x (Numeric 0))}
+
+
+-- TODO this needs to be recursive
+--makeBombField :: Int -> MineField -> StdGen -> MineField
+--makeBombField b old g = (\x -> updateTile x bomb old) bombs
+--          where bombs = makeShuffledCoordinates old g b
+
+updateTile :: Pos -> Tile -> MineField -> MineField
+updateTile (x,y) val field = MineField(x' ++ [xRow !!= (x,val)] ++ x'')
+              where xs  = rows field
+                    x'   = take y xs
+                    xRow = concat (take 1 (drop y xs))
+                    x''  = drop (y+1) xs
+
+(!!=) :: [a] -> (Int,a) -> [a]
+(!!=) xs (pos,val) = take pos xs ++ [val] ++ drop (pos+1) xs
+
+makeShuffledCoordinates :: [Pos] -> StdGen -> Int -> [Pos]
+makeShuffledCoordinates coords g n = take n shuffled
+                  where len = length coords
+                        shuffled = shuffle' coords len g
+
+
+makeCoordinates :: Int -> Int -> [Pos]
+makeCoordinates x y = [(x,y) | y <- [0..(y-1)], x <- [0..(x-1)]]
+
+calculateField = undefined
+
+getPos = undefined
---
+++ MineSweeper/README.txt
@@ -0,0 +1,9 @@
+Need package, install with:
+cabal install random-shuffle
+
+use with:
+shuffle' list (length list) StdGen
+^ doesn't seem to return new StdGen? but doesn't matter in our case
+
+Link:
+https://hackage.haskell.org/package/random-shuffle-0.0.4/docs/System-Random-Shuffle.html

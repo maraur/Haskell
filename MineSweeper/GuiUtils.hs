94080c8c445c6cd392c4ec372c9f3f1a60660cf7
Marcus Aurén <auren@student.chalmers.se>
2016-12-12 12:58:42 +0100

    Updated guiUtils

--- MineSweeper/GuiUtils.hs
+++ MineSweeper/GuiUtils.hs
@@ -8,16 +8,25 @@ data ViewTile = B | H | F | Printing Int
           deriving (Show, Eq)
 
 --dataToGui :: GuiMineField -> [ViewTile]
-dataToGui field = concat (map lineToGui (rows' field))
+dataToGui field = concat [lineToGui i (fieldRows !! i) | i <- [0..len]]
+       where fieldRows = rows' field
+             len       = length (fieldRows) - 1
+
 
 --lineToGui :: [GuiTile] -> [ViewTile]
-lineToGui = map tileToGui
+lineToGui line xs = map tileToGui zipList
+       where zipList = zip xs posList
+             posList = makePosList line xs
+
+makePosList :: Int -> [a] -> [Pos]
+makePosList line xs = [(x,line) | x <- [0..len]]
+       where len = (length xs) - 1
 
 --tileToGui :: GuiTile -> (ViewTile, F t)
-tileToGui (Nonvisible, _) = (H, buttonF (tileLabel H))
-tileToGui (Flag, _) = (F, buttonF (tileLabel F))
-tileToGui (_, Bomb) = (B, buttonF (tileLabel B))
-tileToGui (_, Numeric i) = (Printing i, buttonF (show i))
+tileToGui ((Nonvisible, _), pos) = (pos, buttonF (tileLabel H))
+tileToGui ((Flag, _), pos) = (pos, buttonF (tileLabel F))
+tileToGui ((_, Bomb), pos) = (pos, buttonF (tileLabel B))
+tileToGui ((_, Numeric i), pos) = (pos, buttonF (show i))
 
 tileLabel H = " "
 tileLabel F = "F"

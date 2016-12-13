d77c53b2aa4a7a7558b4471f4c24b6a7ad770faa
gudruney9 <gudruney9@gmail.com>
2016-12-13 16:49:06 +0100

    randomized field in main

--- MineSweeper/Gui.hs
+++ MineSweeper/Gui.hs
@@ -2,18 +2,23 @@ import WebFudgets
 import Haste.Graphics.Canvas
 import MineSweeper
 import GuiUtils
-{-
-data Tile = Bomb | Numeric Int
-              deriving (Show, Eq)
--}
-main = runF minefield
+import System.Random
+import Haste.DOM
+import Haste.Events
+import Data.IORef
+
+--main :: IO()
+main = do g <- newStdGen
+          runF (minefield g)
+
 
 shellF title fud = boxF (h2F (textF title) >+ fud)
 
-minefield = shellF "Minesweeper" $
+minefield g = shellF "Minesweeper" $
     showF =<= fieldF
   where
-    field = makeNewGuiField 15 15
-    viewField = dataToGui field
-    fieldF = tableF (length (rows' field)) buttonsF
+    field = calculateField (makeBombField 10 (makeEmptyField 9 9) g)
+    guiField = makeGuiField field
+    viewField = dataToGui guiField
     buttonsF = listF viewField
+    fieldF = tableF (length (rows' guiField)) buttonsF

e0afd1a4d33bf7617107174b8a1ec719ab60a611
gudruney9 <gudruney9@gmail.com>
2016-12-13 21:55:39 +0100

    new button on top that toggles flag and press
    alert when player hits bomb.

--- MineSweeper/Test.hs
+++ MineSweeper/Test.hs
@@ -14,6 +14,10 @@ main = do
     let field = calculateField (makeBombField 7 (makeEmptyField 5 5) g)
     --let coords = makeCoordinates field
     --let buttons = mkButtons field
+    actionButton <- newElem "button"
+    set actionButton [ prop "innerHTML" =: "Press"]
+
+
     button1 <- mkButton  " " (0,0)
     button2 <- mkButton  " " (1,0)
     button3 <- mkButton  " " (2,0)
@@ -40,53 +44,76 @@ main = do
     button24 <- mkButton " " (3,4)
     button25 <- mkButton " " (4,4)
 
+    topRow   <- newElem "div"
     row1  <- newElem "div"
     row2  <- newElem "div"
     row3  <- newElem "div"
     row4  <- newElem "div"
     row5  <- newElem "div"
 
+    row topRow [actionButton]
     row row1  [button1, button2, button3, button4, button5]
     row row2  [button6, button7, button8, button9, button10]
     row row3  [button11, button12, button13, button14, button15]
     row row4  [button16, button17, button18, button19, button20]
     row row5  [button21, button22, button23, button24, button25]
 
-    let buttons = [row1, row2, row3, row4, row5]
+    let buttons = [topRow, row1, row2, row3, row4, row5]
 
     column documentBody buttons
 
-    onEvent button1 Click $ \_ -> do update button1 field
-    onEvent button2 Click $ \_ -> do update button2 field
-    onEvent button3 Click $ \_ -> do update button3 field
-    onEvent button4 Click $ \_ -> do update button4 field
-    onEvent button5 Click $ \_ -> do update button5 field
-    onEvent button6 Click $ \_ -> do update button6 field
-    onEvent button7 Click $ \_ -> do update button7 field
-    onEvent button8 Click $ \_ -> do update button8 field
-    onEvent button9 Click $ \_ -> do update button9 field
-    onEvent button10 Click $ \_ -> do update button10 field
-    onEvent button11 Click $ \_ -> do update button11 field
-    onEvent button12 Click $ \_ -> do update button12 field
-    onEvent button13 Click $ \_ -> do update button13 field
-    onEvent button14 Click $ \_ -> do update button14 field
-    onEvent button15 Click $ \_ -> do update button15 field
-    onEvent button16 Click $ \_ -> do update button16 field
-    onEvent button17 Click $ \_ -> do update button17 field
-    onEvent button18 Click $ \_ -> do update button18 field
-    onEvent button19 Click $ \_ -> do update button19 field
-    onEvent button20 Click $ \_ -> do update button20 field
-    onEvent button21 Click $ \_ -> do update button21 field
-    onEvent button22 Click $ \_ -> do update button22 field
-    onEvent button23 Click $ \_ -> do update button23 field
-    onEvent button24 Click $ \_ -> do update button24 field
-    onEvent button25 Click $ \_ -> do update button25 field
+    onEvent actionButton Click $ \_ -> do toggleAction actionButton
 
-update btn field = do
-                      strPos <- getProp btn "btnID"
-                      let el = elToString (getElement (toPos strPos) field)
-                      set btn [ prop "innerHTML" =: el,  prop "style" =: "width:2em;height:2em"]
-                      return ()
+    onEvent button1 Click $ \_ -> do update button1 field actionButton
+    onEvent button2 Click $ \_ -> do update button2 field actionButton
+    onEvent button3 Click $ \_ -> do update button3 field actionButton
+    onEvent button4 Click $ \_ -> do update button4 field actionButton
+    onEvent button5 Click $ \_ -> do update button5 field actionButton
+    onEvent button6 Click $ \_ -> do update button6 field actionButton
+    onEvent button7 Click $ \_ -> do update button7 field actionButton
+    onEvent button8 Click $ \_ -> do update button8 field actionButton
+    onEvent button9 Click $ \_ -> do update button9 field actionButton
+    onEvent button10 Click $ \_ -> do update button10 field actionButton
+    onEvent button11 Click $ \_ -> do update button11 field actionButton
+    onEvent button12 Click $ \_ -> do update button12 field actionButton
+    onEvent button13 Click $ \_ -> do update button13 field actionButton
+    onEvent button14 Click $ \_ -> do update button14 field actionButton
+    onEvent button15 Click $ \_ -> do update button15 field actionButton
+    onEvent button16 Click $ \_ -> do update button16 field actionButton
+    onEvent button17 Click $ \_ -> do update button17 field actionButton
+    onEvent button18 Click $ \_ -> do update button18 field actionButton
+    onEvent button19 Click $ \_ -> do update button19 field actionButton
+    onEvent button20 Click $ \_ -> do update button20 field actionButton
+    onEvent button21 Click $ \_ -> do update button21 field actionButton
+    onEvent button22 Click $ \_ -> do update button22 field actionButton
+    onEvent button23 Click $ \_ -> do update button23 field actionButton
+    onEvent button24 Click $ \_ -> do update button24 field actionButton
+    onEvent button25 Click $ \_ -> do update button25 field actionButton
+
+toggleAction btn = do
+            action <- getProp btn "innerHTML"
+            if action == "Press" then do
+                set btn [ prop "innerHTML" =: "Flag"]
+            else do
+                set btn [ prop "innerHTML" =: "Press"]
+            return ()
+
+
+--TODO make a isFlag function
+update btn field actionBtn = do
+      strPos <- getProp btn "btnID"
+      let tile = getElement (toPos strPos) field
+      let el = elToString tile
+      action <- getProp actionBtn "innerHTML"
+      if action == "Flag" then do
+        set btn [ prop "innerHTML" =: "F"]
+      else do
+        if tile == Bomb then do
+          set btn [ prop "innerHTML" =: el,  prop "style" =: "width:2em;height:2em"]
+          alert "Game Over!"
+        else do
+          set btn [ prop "innerHTML" =: el,  prop "style" =: "width:2em;height:2em"]
+          return ()
 
 --mkButtons :: MineField -> [IO Elem]
 --mkButtons field = [mkButton (getElement pos field) pos | pos <- coords ]
